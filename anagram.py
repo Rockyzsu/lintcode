b2a3b3843593a65d4ebc86ad5cf95ae6cc27b4d0
Rockyzsu <jinweizsu@gmail.com>
2017-02-28 16:58:41 +0800

    anagram

---
+++ anagram.py
@@ -0,0 +1,16 @@
+#-*-coding=utf-8-*-
+__author__ = 'xda'
+'''
+wrong alghim:
+a='aac'
+b='acc'
+def anagram(s,t):
+    for i in s:
+        if i not in t:
+            return False
+    return True
+'''
+a='abc'
+b='cba'
+c='aaa'
+print anagram(a,c)
